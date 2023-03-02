b71d55cc2237f6b223f53b6862eef51184af1457
Sherylleong <sherylleongkitye@gmail.com>
2023-03-02 12:51:07 +0800

    added filters in fieldsets so easier to style

--- my-app/src/Components/EventMap/EventMap.js
+++ my-app/src/Components/EventMap/EventMap.js
@@ -1,7 +1,7 @@
 import { Component, useMemo } from "react"
 import { GoogleMap, useLoadScript, Marker, MarkerClusterer} from '@react-google-maps/api';
 import "./event-map-styles.css"
-
+import Filters from "../Filters/Filters.js"
 export default function MapContainer(){
 	const {isLoaded} = useLoadScript({
 		googleMapsApiKey: "AIzaSyCGCznAwZAFJ8qMQY1ckg6EfDwuczmepWI",
@@ -9,56 +9,6 @@ export default function MapContainer(){
 
 	if (!isLoaded) return <div>..Loading</div>
 	return <Map />
-}
-
-function filters(){
-	let groups = ["a","b"];
-	function handleFilters(){}
-	return (
-		<form>
-		  <select
-			name="filterIntensity"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-		  </select>
-		  <label>Date</label>
-		  <label>Start Date</label>
-		  <input type="date"/>
-		  <label>End Date</label>
-		  <input type="date"/>
-
-		  <select
-			name="filterCategory"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-			<option value="none">Category</option>
-			<option value="Walking"> Running</option>
-			<option value="Jogging"> Running</option>
-			<option value="Running"> Running</option>
-			<option value="Climbing"> Running</option>
-			<option value="Biking"> Running</option>
-			<option value="Sports"> Running</option>
-			<option value="Other"> Running</option>
-		  </select>
-
-		  <select
-			name="filterGroup"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-			{groups.map(group => {
-           		return (
-             		<option value={group}> {group} </option>
-           		)
-         	})}
-		  </select>
-		</form>
-	  );
 }
 
 function Map() {
@@ -80,7 +30,9 @@ function Map() {
 	// markerLoc.push({lat: 1.3458889951752477, lng: 103.69933201581368});
 	// markerLoc.push({lat: 1.3451843416350524, lng: 103.69917596458055});
 	return (
+		
 		<>
+			<Filters/>
 			<GoogleMap zoom={18} 
 			center={markers[1].position} 
 			mapContainerClassName="map-container">
--- my-app/src/Components/Filters/Filters.js
+++ my-app/src/Components/Filters/Filters.js
@@ -1,51 +1,52 @@
-
-
-function filters(){
+export default function Filters(){
 	let groups = ["a","b"];
 	function handleFilters(){}
 	return (
 		<form>
-		  <select
-			name="filterIntensity"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-		  </select>
-		  <label>Date</label>
-		  <label>Start Date</label>
-		  <input type="date"/>
-		  <label>End Date</label>
-		  <input type="date"/>
-
-		  <select
-			name="filterCategory"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-			<option value="none">Category</option>
-			<option value="Walking"> Running</option>
-			<option value="Jogging"> Running</option>
-			<option value="Running"> Running</option>
-			<option value="Climbing"> Running</option>
-			<option value="Biking"> Running</option>
-			<option value="Sports"> Running</option>
-			<option value="Other"> Running</option>
-		  </select>
-
-		  <select
-			name="filterGroup"
-			onChange={(e) => {
-				handleFilters(e);
-			}}
-		  >
-			{groups.map(group => {
+            <fieldset>
+                <legend>Intensity</legend>
+                <input type="checkbox" name="Intensity" value="Low" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Low">Low</label>
+                <input type="checkbox" name="Intensity" value="Moderate" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Moderate">Moderate</label>
+                <input type="checkbox" name="Intensity" value="Vigorous" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Vigorous">Vigorous</label>
+            </fieldset>
+            <fieldset>
+                <legend>Date</legend>
+                <label>Start Date</label>
+                <input type="date" onChange={(e) => {handleFilters(e);}}/>
+                <label>End Date</label>
+                <input type="date" onChange={(e) => {handleFilters(e);}}/>
+            </fieldset>
+            <fieldset>
+                <legend>Category</legend>
+                <input type="checkbox" name="Category" value="Walking" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Walking">Walking</label>
+                <input type="checkbox" name="Category" value="Jogging" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Jogging">Jogging</label>
+                <input type="checkbox" name="Category" value="Running" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Running">Running</label>
+                <input type="checkbox" name="Category" value="Climbing" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Climbing">Climbing</label>
+                <input type="checkbox" name="Category" value="Biking" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Biking">Biking</label>
+                <input type="checkbox" name="Category" value="Sports" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Sports">Sports</label>
+                <input type="checkbox" name="Category" value="Other" onChange={(e) => {handleFilters(e);}}/>
+                <label for="Other">Other</label>
+            </fieldset>
+            <fieldset>
+                <legend>Group</legend>
+                {groups.map(group => {
            		return (
-             		<option value={group}> {group} </option>
+                    <>
+                    <input type="checkbox" name="Group" value={group} onChange={(e) => {handleFilters(e);}}/>
+                    <label for="Group">{group}</label>
+                    </>
            		)
          	})}
-		  </select>
+            </fieldset>
 		</form>
 	  );
 }
